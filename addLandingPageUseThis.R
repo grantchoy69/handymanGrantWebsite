0a75cbe13b6d1bf35c987d46c81200020175d3eb
grantchoy69 <grantchoy@gmail.com>
2025-11-24 19:48:10 -0800

    adding tags to all landing pages

--- addLandingPageUseThis.R
+++ addLandingPageUseThis.R
@@ -6,3 +6,12 @@ generateLandingPage(
   pageSlug   = "drywall-repair",
   jsonPath   = "drywallRepair.json"
 )
+
+
+generateLandingPage("index.html", "drywall-repair", "drywallRepair.json")
+generateLandingPage("index.html", "faucet-installation", "faucetInstallation.json")
+generateLandingPage("index.html", "toilet-installation", "toiletInstallation.json")
+generateLandingPage("index.html", "light-fixture-installation", "lightFixtureInstallation.json")
+generateLandingPage("index.html", "ceiling-fan-installation", "ceilingFanInstallation.json")
+generateLandingPage("index.html", "door-repair", "doorRepair.json")
+generateLandingPage("index.html", "fence-repair", "fenceRepair.json")
